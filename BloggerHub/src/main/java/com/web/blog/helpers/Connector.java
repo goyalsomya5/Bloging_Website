e20ddbdb8bd8fe7d4da320741f0816a078f08f1a
INDIA <INDIA@DESKTOP-FE2HGSI>
2021-06-20 03:08:16 +0530

    f

---
+++ BloggerHub/src/main/java/com/web/blog/helpers/Connector.java
@@ -0,0 +1,21 @@
+package com.web.blog.helpers;
+
+import java.sql.*;
+public class Connector {
+	private static Connection con;
+
+	public static Connection getConnection() {
+		
+		try {
+			System.out.println(con);
+			if (con == null) {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bloggerhub", "root", "harvestgold");
+				
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
